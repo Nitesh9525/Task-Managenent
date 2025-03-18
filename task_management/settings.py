a68120bad9b6368448bcb67bb46d3e3d7a2f83d4
nitesh <singhnitesh56789@gmail.com>
2025-03-18 12:50:51 +0530

    use drf_yasg

--- task_management/settings.py
+++ task_management/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'drf_yasg',
+    'drf_yasg',
     'task',
-    'drf_spectacular',
+    # 'drf_spectacular',
     'django_filters',
 ]
 
